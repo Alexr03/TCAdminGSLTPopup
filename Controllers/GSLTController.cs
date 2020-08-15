14a5e53057c52818ba13ae7ebd3b889c23f3934c
Alex Redding <Alexredding180@gmail.com>
2020-08-15 22:07:59 +0100

    Initial

---
+++ Controllers/GSLTController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using TCAdmin.GameHosting.SDK.Objects;
+using TCAdmin.SDK.Web.MVC.Controllers;
+using TCAdmin.Web.MVC;
+
+namespace TCAdminGSLT.Controllers
+{
+    [ExceptionHandler]
+    public class GsltController : BaseServiceController
+    {
+        [HttpPost]
+        [ParentAction("Service", "Home")]
+        public ActionResult SetToken(string token)
+        {
+            var service = Service.GetSelectedService();
+            service.Variables["GSLT"] = token;
+            service.Save();
+            service.Configure();
+
+            return Json(new
+            {
+                Message = "Successfully saved GSLT."
+            });
+        }
+    }
+}
